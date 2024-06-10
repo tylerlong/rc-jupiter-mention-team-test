4419d62b4e42c7dc5bf154671e3eaa077282df89
Tyler Liu <tyler4long@gmail.com>
2024-06-10 13:35:25 -0700

    Done the experiment

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Untitled App
+# rc-jupiter-mention-team-test
+
+This is a test repository to test the mention team feature in RingCentral app.
+
+
+## notification
+Manually post a `@Team`
+
+```
+"text": "<a class='at_mention_compose' rel='{\"id\":-1}'>@Team</a> ",
+```
+
+## Post a message with `@Team` in the text
+
+Tried the following:
+
+`![:Team](team-id)`: the team is not highlighted in yellow
+
+`![:Team](-1)`: link to -1, not what we want
+
+`"<a class='at_mention_compose' rel='{\"id\":-1}'>@Team</a>"`: shows a link to the team, but the link does redirect. 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log(`Hello ${process.env.NAME}!`);
+import RingCentral from '@rc-ex/core';
+// import WebSocketExtension from '@rc-ex/ws';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  await rc.authorize({
+    jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
+  });
+  // const wsExtension = new WebSocketExtension();
+  // await rc.installExtension(wsExtension);
+  // await wsExtension.subscribe(['/team-messaging/v1/posts'], (event) => {
+  //   console.log(JSON.stringify(event, null, 2));
+  // });
+
+  await rc.teamMessaging().v1().chats('86913122310').posts().post({
+    text: "<a class='at_mention_compose' rel='{\"id\":-1}'>@Team</a>",
+  });
+};
+main();
